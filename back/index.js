5f7ef043872e8e76ef2e74e7bd49f3bfd2c8ab62
NadiaPoi <nadia.poi.fou@gmail.com>
2019-12-09 13:20:01 +0100

    add backend and start front app
    	new file:   .expo-shared/assets.json
    	new file:   .gitignore
    	new file:   App.js
    	new file:   app.json
    	new file:   assets/icon.png
    	new file:   assets/splash.png
    	new file:   babel.config.js
    	new file:   back/app.js
    	new file:   back/index.js
    	new file:   back/models/Post.js
    	new file:   back/models/User.js
    	new file:   back/package-lock.json
    	new file:   back/package.json
    	new file:   back/router.js
    	new file:   back/scraper.js
    	new file:   package-lock.json
    	new file:   package.json

---
+++ back/index.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const app = require("./app");
+
+// Multiples connections
+const dbConnect = mongoose
+  .set("useCreateIndex", true)
+  .connect("mongodb://localhost:27017/default", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+dbConnect
+  .then(() => {
+    const port = 8800;
+
+    app.listen(port, err => {
+      if (err) {
+        console.error(err);
+      }
+      console.log("Express server listening on port 8800.");
+    });
+  })
+  .catch(err => {
+    console.error("Error DB connecting", { err });
+  });
