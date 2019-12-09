06daa099674de79bb73d084cf115f4c585ca548b
NadiaPoi <nadia.poi.fou@gmail.com>
2019-12-09 13:20:01 +0100

    add backend and start front app

---
+++ back/router.js
@@ -0,0 +1,156 @@
+const sanitize = require("mongo-sanitize");
+const bcrypt = require("bcryptjs");
+
+const Post = require("./models/Post");
+const User = require("./models/User");
+const getMetaProperties = require("./scraper");
+
+exports.init = app => {
+  app.get("/users", async (req, res) => {
+    const users = await User.find();
+    return res.status(200).send(users);
+  });
+
+  app.get("/posts", async (req, res) => {
+    const posts = await Post.find();
+    return res.status(200).send(posts);
+  });
+
+  app.get("/post/:id", async (req, res) => {
+    const postId = req.params.id;
+    const post = await Post.findOne({ _id: { $in: [postId] } });
+    return res.status(200).send(`Only One post : ${post}`);
+  });
+
+  app.post("/posts", async (req, res) => {
+    const url = sanitize(req.body.url);
+
+    if (!url) {
+      return res.status(400).send({
+        text: "Bad request"
+      });
+    }
+
+    try {
+      const findPost = await Post.findOne({ url: { $in: [url] } });
+      if (findPost) {
+        return res.status(405).send({
+          text: "Post already exist"
+        });
+      }
+    } catch (error) {
+      return res.status(500).send({ error });
+    }
+
+    const post = { url, ...(await getMetaProperties(url)) };
+
+    try {
+      const postData = new Post(post);
+      const postSaved = await postData.save();
+
+      return res.status(201).send({
+        text: "Succes, new post created"
+      });
+    } catch (error) {
+      return res.status(500).send({ error });
+    }
+  });
+
+  app.delete("/posts/:id", async (req, res) => {
+    try {
+      if (req.params.id) {
+        const postId = req.params.id;
+        await Post.deleteOne({ _id: postId });
+        return res.status(200).redirect("/posts");
+      }
+    } catch (error) {
+      return res.status(500).send({ error });
+    }
+  });
+
+  app.post("/signup", async (req, res) => {
+    const username = sanitize(req.body.username);
+    const email = sanitize(req.body.email);
+    const password = sanitize(req.body.password);
+
+    if (!username || !email || !password) {
+      return res.status(400).send({
+        text: "Bad request"
+      });
+    }
+
+    try {
+      const findUser = await User.findOne({ email: { $in: [email] } });
+      if (findUser) {
+        return res.status(405).send({
+          text: "User already exist"
+        });
+      }
+    } catch (error) {
+      return res.status(500).send({ error });
+    }
+
+    // Sauvegarde de l'utilisateur en base et hashage du mot de passe
+    try {
+      const user = new User({
+        username,
+        email,
+        password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user.password, salt, async function(err, hashedPassword) {
+          if (err) {
+            console.error(err);
+          }
+          user.password = hashedPassword;
+          const userSaved = await user.save();
+
+          return res.status(201).send({
+            text: "Succes, new user saved",
+            token: userSaved.getToken(userSaved)
+          });
+        });
+      });
+    } catch (error) {
+      return res.status(500).send({ error });
+    }
+  });
+
+  app.post("/login", async (req, res) => {
+    const email = sanitize(req.body.email);
+    const password = sanitize(req.body.password);
+
+    if (!email || !password) {
+      return res.status(400).send({
+        text: "Bad request"
+      });
+    }
+
+    try {
+      const findUser = await User.findOne({ email: { $in: [email] } });
+      const userPassw = findUser.password;
+
+      findUser.authenticate(password, userPassw, (err, samePassword) => {
+        if (!samePassword) {
+          return res.status(401).send({
+            text: "Wrong password"
+          });
+        }
+
+        if (samePassword) {
+          const token = findUser.getToken(findUser);
+          req.session.token = token;
+          return res.status(200).send({
+            token: req.session.token,
+            text: "Succes Authentication"
+          });
+        }
+      });
+    } catch (error) {
+      return res.status(404).send({
+        text: "User's not exist"
+      });
+    }
+  });
+};
